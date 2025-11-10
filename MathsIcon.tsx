6c35060a03c712e705adbad21b0fab2f40b39633
sjajala22 <sireeshajajala@gmail.com>
2025-11-10 12:31:40 +0000

    Add files via upload

---
+++ MathsIcon.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+const MathsIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <line x1="18" y1="6" x2="6" y2="18" />
+    <line x1="6" y1="6" x2="18" y2="18" />
+    <circle cx="12" cy="12" r="10" />
+  </svg>
+);
+
+export default MathsIcon;
